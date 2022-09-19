30e6729b7c076aad2459683f200d76349fcea7ce
Joakim Skoglund <joakim@voltura.se>
2022-09-19 19:51:43 +0200

    Add project files.

---
+++ DotnetCoreMVC_with_TS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetCoreMVC_with_TS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
